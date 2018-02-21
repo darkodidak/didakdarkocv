3fc5df434884c2be256fdf44a43462c9b79b26c7
darkodidak <darkodidak1@gmail.com>
2018-02-21 12:41:15 +0100

    Importing project

---
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication1
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+               bundles.Add(new StyleBundle("~/bundles/css")
+                .Include(
+                "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Content/vendor/font-awesome/css/font-awesome.min.css",
+                "~/Content/vendor/devicons/css/devicons.min.css",
+                "~/Content/vendor/simple-line-icons/css/simple-line-icons.css",
+                "~/Content/css/resume.min.css"));
+
+
+  
+
+               bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include(
+                "~/Content/vendor/jquery/jquery.min.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/jquery-easing/jquery.easing.min.js",
+                "~/Content/js/resume.min.js"));
+        }
+    }
+}
